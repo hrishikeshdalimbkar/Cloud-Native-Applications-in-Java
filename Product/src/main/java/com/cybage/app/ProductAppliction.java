669164c33391c66c9fda6c4666859c0dfa326fdb
hrishikesh <hrishikeshd@cybage.com>
2021-12-22 16:09:40 +0530

    Project Product Structure Created

---
+++ Product/src/main/java/com/cybage/app/ProductAppliction.java
@@ -0,0 +1,13 @@
+package com.cybage.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAppliction {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAppliction.class, args);
+	}
+
+}
