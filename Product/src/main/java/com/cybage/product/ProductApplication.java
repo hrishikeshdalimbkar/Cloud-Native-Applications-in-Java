20008a482f8e91048d821d8878d79e322b726b58
hrishikesh <hrishikeshd@cybage.com>
2021-12-23 12:22:16 +0530

    Product Service Reinitiated with spring starter project

--- Product/src/main/java/com/cybage/product/ProductApplication.java
+++ Product/src/main/java/com/cybage/product/ProductApplication.java
@@ -3,11 +3,11 @@ package com.cybage.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.cybage")
-public class ProductAppliction {
+@SpringBootApplication
+public class ProductApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductAppliction.class, args);
+		SpringApplication.run(ProductApplication.class, args);
 	}
 
 }
