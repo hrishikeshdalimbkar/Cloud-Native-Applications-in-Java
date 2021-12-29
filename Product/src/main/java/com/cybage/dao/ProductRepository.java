344a8f25cad3713cd0809041d004ca54e16c0e96
hrishikesh <hrishikeshd@cybage.com>
2021-12-29 18:21:06 +0530

    Cache Evict Implemented: After Product CRUD operations : POST, PUT, DELETE the cache will update

--- Product/src/main/java/com/cybage/dao/ProductRepository.java
+++ Product/src/main/java/com/cybage/dao/ProductRepository.java
@@ -4,6 +4,7 @@ import com.cybage.pojo.Product;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,5 +14,11 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 	@Cacheable("productsByCategoryCache")
 	List<Product> findByCatId(int catId);
+
+	@CacheEvict(cacheNames = "productsByCategoryCache", key = "#result?.catId")
+	Product save(Product product);
+
+	@CacheEvict(cacheNames = "productsByCategoryCache", key = "#p0.catId")
+	void delete(Product product);
 
 }
